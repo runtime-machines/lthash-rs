357eed892e20c6c2bbfa7c00b5e3a70a273568c1
ripa1995 <roberto.ripamonti@ymail.com>
2023-01-26 10:47:56 +0100

    test: lthash32

---
+++ tests/lthash16.rs
@@ -0,0 +1,115 @@
+use lthash_rs::LtHash;
+use sha3::Shake128;
+
+const LORUM: &str = include_str!("./test-data/lorum-ipsum.txt");
+const HASH: &str = include_str!("./test-data/lorum-hash-16.txt");
+
+type LtHash16 = lthash_rs::LtHash16<Shake128>;
+
+#[test]
+fn insert_remove_object() {
+    let mut lthash = LtHash16::new();
+    let elements = ["apple", "banana", "kiwi"];
+    lthash.insert(elements[0]);
+    lthash.insert(elements[1]);
+    lthash.insert(elements[2]);
+    lthash.remove(elements[1]);
+    let mut lthash_bis = LtHash16::new();
+    lthash_bis.insert(elements[0]);
+    lthash_bis.insert(elements[2]);
+    assert_eq!(lthash.into_bytes(), lthash_bis.into_bytes());
+}
+
+#[test]
+fn insert_with_extend_remove_object() {
+    let mut lthash = LtHash16::new();
+
+    let objects = vec!["apple", "banana", "kiwi"];
+    lthash.extend(&objects);
+    lthash.remove(b"banana");
+
+    let mut lthash_bis = LtHash16::new();
+    let objects = vec!["apple", "kiwi"];
+    lthash_bis.extend(&objects);
+    assert_eq!(lthash, lthash_bis, "values don't match");
+}
+
+#[test]
+fn lorum_ipsum_insert() {
+    let mut lthash = LtHash16::new();
+    for object in LORUM.lines() {
+        lthash.insert(object);
+    }
+
+    assert_eq!(lthash.to_hex_string(), HASH.trim());
+}
+
+#[test]
+fn lorum_ipsum_insert_with_extend() {
+    let mut lthash = LtHash16::new();
+
+    lthash.extend(LORUM.lines());
+
+    assert_eq!(lthash.to_hex_string(), HASH.trim());
+}
+
+#[test]
+fn union() {
+    let mut left = LtHash16::new();
+    left.insert("hello");
+
+    let mut right = LtHash16::new();
+    right.insert("world");
+
+    assert_eq!(left.union(&right), LtHash16::from_iter(["hello", "world"]));
+}
+
+#[test]
+fn bitor() {
+    let mut left = LtHash16::new();
+    left.insert("hello");
+
+    let mut right = LtHash16::new();
+    right.insert("world");
+
+    assert_eq!(&left | &right, LtHash16::from_iter(["hello", "world"]));
+    assert_eq!(left | right, LtHash16::from_iter(["hello", "world"]));
+}
+
+#[test]
+fn difference() {
+    let mut left = LtHash16::new();
+    left.extend(["hello", "world"]);
+
+    let mut right = LtHash16::new();
+    right.insert("world");
+
+    assert_eq!(left.difference(&right), LtHash16::from_iter(["hello"]));
+}
+
+#[test]
+fn sub() {
+    let mut left = LtHash16::new();
+    left.extend(["hello", "world"]);
+
+    let mut right = LtHash16::new();
+    right.insert("world");
+
+    assert_eq!(&left - &right, LtHash16::from_iter(["hello"]));
+    assert_eq!(left - right, LtHash16::from_iter(["hello"]));
+}
+
+#[test]
+fn into_from_bytes() {
+    let mut left = LtHash16::new();
+    left.extend(["hello", "world"]);
+
+    let bytes = left.into_bytes();
+
+    let right = LtHash16::try_from(bytes.as_ref()).unwrap();
+
+    let mut left = LtHash16::new();
+    left.extend(["hello", "world"]);
+
+    assert_eq!(left, right);
+}
---
+++ tests/lthash32.rs
@@ -0,0 +1,115 @@
+use lthash_rs::LtHash;
+use sha3::Shake128;
+
+const LORUM: &str = include_str!("./test-data/lorum-ipsum.txt");
+const HASH: &str = include_str!("./test-data/lorum-hash-32.txt");
+
+type LtHash32 = lthash_rs::LtHash32<Shake128>;
+
+#[test]
+fn insert_remove_object() {
+    let mut lthash = LtHash32::new();
+    let elements = ["apple", "banana", "kiwi"];
+    lthash.insert(elements[0]);
+    lthash.insert(elements[1]);
+    lthash.insert(elements[2]);
+    lthash.remove(elements[1]);
+    let mut lthash_bis = LtHash32::new();
+    lthash_bis.insert(elements[0]);
+    lthash_bis.insert(elements[2]);
+    assert_eq!(lthash.into_bytes(), lthash_bis.into_bytes());
+}
+
+#[test]
+fn insert_with_extend_remove_object() {
+    let mut lthash = LtHash32::new();
+
+    let objects = vec!["apple", "banana", "kiwi"];
+    lthash.extend(&objects);
+    lthash.remove(b"banana");
+
+    let mut lthash_bis = LtHash32::new();
+    let objects = vec!["apple", "kiwi"];
+    lthash_bis.extend(&objects);
+    assert_eq!(lthash, lthash_bis, "values don't match");
+}
+
+#[test]
+fn lorum_ipsum_insert() {
+    let mut lthash = LtHash32::new();
+    for object in LORUM.lines() {
+        lthash.insert(object);
+    }
+
+    assert_eq!(lthash.to_hex_string(), HASH.trim());
+}
+
+#[test]
+fn lorum_ipsum_insert_with_extend() {
+    let mut lthash = LtHash32::new();
+
+    lthash.extend(LORUM.lines());
+
+    assert_eq!(lthash.to_hex_string(), HASH.trim());
+}
+
+#[test]
+fn union() {
+    let mut left = LtHash32::new();
+    left.insert("hello");
+
+    let mut right = LtHash32::new();
+    right.insert("world");
+
+    assert_eq!(left.union(&right), LtHash32::from_iter(["hello", "world"]));
+}
+
+#[test]
+fn bitor() {
+    let mut left = LtHash32::new();
+    left.insert("hello");
+
+    let mut right = LtHash32::new();
+    right.insert("world");
+
+    assert_eq!(&left | &right, LtHash32::from_iter(["hello", "world"]));
+    assert_eq!(left | right, LtHash32::from_iter(["hello", "world"]));
+}
+
+#[test]
+fn difference() {
+    let mut left = LtHash32::new();
+    left.extend(["hello", "world"]);
+
+    let mut right = LtHash32::new();
+    right.insert("world");
+
+    assert_eq!(left.difference(&right), LtHash32::from_iter(["hello"]));
+}
+
+#[test]
+fn sub() {
+    let mut left = LtHash32::new();
+    left.extend(["hello", "world"]);
+
+    let mut right = LtHash32::new();
+    right.insert("world");
+
+    assert_eq!(&left - &right, LtHash32::from_iter(["hello"]));
+    assert_eq!(left - right, LtHash32::from_iter(["hello"]));
+}
+
+#[test]
+fn into_from_bytes() {
+    let mut left = LtHash32::new();
+    left.extend(["hello", "world"]);
+
+    let bytes = left.into_bytes();
+
+    let right = LtHash32::try_from(bytes.as_ref()).unwrap();
+
+    let mut left = LtHash32::new();
+    left.extend(["hello", "world"]);
+
+    assert_eq!(left, right);
+}
